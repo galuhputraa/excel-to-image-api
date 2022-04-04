c14f8e822f1a304b694217d686fad894260d9374
Galuh Putra Warman <galuhputraww@gmail.com>
2022-04-04 12:38:53 +0700

    add cors

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config()
 var indexRouter = require('./routes/index');
+var cors = require('cors')
+ 
+
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
